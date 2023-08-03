291678a49686cda79e766c67eebcb2268a90ee9d
Davis Weimer <davisweimer@gmail.com>
2023-08-03 17:23:43 -0600

    joins table and bulk_discount.rb

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,6 +5,7 @@ class Merchant < ApplicationRecord
   has_many :invoices, through: :invoice_items, dependent: :destroy
   has_many :customers, through: :invoices, dependent: :destroy
   has_many :transactions, through: :invoices, dependent: :destroy
+  has_many :bulk_discounts, dependent: :destroy
 
   enum status: [:enabled, :disabled]
 
