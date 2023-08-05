23452c6153c39d4fcf0af8bf0075426b5c187d7c
Davis Weimer <davisweimer@gmail.com>
2023-08-05 11:05:14 -0600

    test: bulk_discounts#edit

--- spec/features/bulk_discounts/show_spec.rb
+++ spec/features/bulk_discounts/show_spec.rb
@@ -26,14 +26,35 @@ RSpec.describe "bulk discounts show", type: :feature do
     before :each do
       @merchant_A = create(:merchant)
       @bulk_discounts = create_list(:bulk_discount, 5, merchant: @merchant_A)
+      @bulk_discount_A = create(:bulk_discount, merchant: @merchant_A, discount_percentage: 0.44, minimum_quantity: 1234)
     end
 
     it "displays button to edit" do
-      bulk_discount_A = create(:bulk_discount, merchant: @merchant_A, discount_percentage: 0.44, minimum_quantity: 1234)
 
-      visit merchant_bulk_discount_path(@merchant_A, bulk_discount_A)
+      visit merchant_bulk_discount_path(@merchant_A, @bulk_discount_A)
 
       expect(page).to have_button("Edit")
+    end
+
+    it "can edit current bulk discount and redirect back to bulk discount show page" do
+
+      visit merchant_bulk_discount_path(@merchant_A, @bulk_discount_A)
+
+      click_button("Edit")
+      expect(current_path).to eq(edit_merchant_bulk_discount_path(@merchant_A, @bulk_discount_A))
+      save_and_open_page
+      within ".merchant_bulk_discount_form" do
+        expect(find_field("bulk_discount[discount_percentage]").value).to eq(@bulk_discount_A.discount_percentage.to_s)
+        expect(find_field("bulk_discount[minimum_quantity]").value).to eq(@bulk_discount_A.minimum_quantity.to_s)
+
+        fill_in "bulk_discount[discount_percentage]", with: 0.25
+        fill_in "bulk_discount[minimum_quantity]", with: 25
+        click_button("Submit")
+      end
+      
+      expect(current_path).to eq(merchant_bulk_discount_path(@merchant_A, @bulk_discount_A))
+      expect(page).to have_content("-25% off")
+      expect(page).to have_content("25")
 
     end
   end
