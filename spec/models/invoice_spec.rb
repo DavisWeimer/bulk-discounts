5a03d5f41c9f8e943221e20ff4aedc4f8c096c94
Davis Weimer <davisweimer@gmail.com>
2023-08-06 16:23:22 -0600

    test: #discounted_revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -23,5 +23,27 @@ RSpec.describe Invoice, type: :model do
 
       expect(@invoice_1.total_revenue).to eq(100)
     end
+
+    it "#discounted_revenue" do
+      @merchant_A = create(:merchant)
+  
+      @bulk_discount_A = create(:bulk_discount, merchant: @merchant_A, discount_percentage: 0.25, minimum_quantity: 5)
+      @bulk_discount_B = create(:bulk_discount, merchant: @merchant_A, discount_percentage: 0.68, minimum_quantity: 15)
+      
+      @item_A = create(:item, merchant: @merchant_A, unit_price: 15)
+      
+      @item_B = create(:item, merchant: @merchant_A, unit_price: 5)
+      
+      @customer = create(:customer)
+      @invoice_A = create(:invoice, customer: @customer, status: :completed)
+      
+      @invoice_item_1 = create(:invoice_item, invoice: @invoice_A, item: @item_A, quantity: 15, unit_price: @item_A.unit_price, status: :shipped)
+      @invoice_item_2 = create(:invoice_item, invoice: @invoice_A, item: @item_B, quantity: 14, unit_price: @item_B.unit_price, status: :shipped)
+      
+      @merchant_A.associate_bulk_discounts
+
+      expect(@invoice_A.total_revenue).to eq(295)
+      expect(@invoice_A.discounted_revenue).to eq(124.5)
+    end
   end
 end
